f116387e69fd3cd31a16e580647936ca93eb2dd8
Lucas Reis <lucasreis_9@hotmail.com>
2022-10-11 04:37:03 -0300

    Finalizada a classe ParkingSpotModel

--- ParkingControl/src/main/java/com/lucasreis/parkingcontrol/ParkingControlApplication.java
+++ ParkingControl/src/main/java/com/lucasreis/parkingcontrol/ParkingControlApplication.java
@@ -4,14 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@RestController
 public class ParkingControlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingControlApplication.class, args);
-		System.out.println("Olá Spring");
+	public static void main(String[] args) { 
+		SpringApplication.run(ParkingControlApplication.class, args);}
+	
+		@GetMapping("/") 
+		public String teste() {
+			return "<h1> Olá Mundo <h1>";
 	}
 
 }
